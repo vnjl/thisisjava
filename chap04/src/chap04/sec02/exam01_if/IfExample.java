c7ca00f4aadf06a55ce4317dc9f28ce617f20e21
vnjl <lsheayenugk1@naver.com>
2022-06-30 10:43:37 +0900

    0630 1042

---
+++ chap04/src/chap04/sec02/exam01_if/IfExample.java
@@ -0,0 +1,19 @@
+package chap04.sec02.exam01_if;
+
+public class IfExample {
+
+	public static void main(String[] args) {
+		int score = 93;
+		
+		if(score>=90) {
+			System.out.println("점수가 90보다 큽니다.");
+			System.out.println("등급은 A 입니다.");
+		}
+		
+		if(score< 90) {  //{} 안 해주면 바로 다음줄까지만 포함
+			System.out.println("점수가 90보다 작습니다.");
+			System.out.println("등급은 B 입니다.");
+		}
+	}
+
+}
