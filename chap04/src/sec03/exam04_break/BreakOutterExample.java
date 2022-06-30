1d0f77d6d3c580639784a359b5f584090c8d9564
vnjl <lsheayenugk1@naver.com>
2022-06-30 14:00:12 +0900

    0630 1232

---
+++ chap04/src/sec03/exam04_break/BreakOutterExample.java
@@ -0,0 +1,18 @@
+package sec03.exam04_break;
+
+public class BreakOutterExample {
+
+	public static void main(String[] args) {
+		outter: for(char upper='A'; upper<='Z'; upper++) {
+			for(char lower='a'; lower<='z'; lower++) {
+				System.out.println(upper + "-" + lower);
+				if(lower=='g') {
+					break outter;
+				}
+			}
+		}
+		System.out.println("프로그램 실행 종료");
+
+	}
+
+}
